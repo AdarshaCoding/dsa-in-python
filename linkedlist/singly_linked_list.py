dd76f72736783615760cf5621428d208b1936fc6
AdarshaCoding <adarshapc4467@gmail.com>
2024-12-19 19:20:04 +0530

    Linked List: Outpute

--- linkedlist/singly_linked_list.py
+++ linkedlist/singly_linked_list.py
@@ -16,3 +16,9 @@ b.next = c
 print(f"a -> b : {id(a.next)}")
 print(f"b -> c : {id(b.next)}")
 print(f"c -> {c.next}")
+
+# Output
+# a: 2159175951872, b: 2159175951920, c: 2159175952016
+# a -> b : 2159175951920
+# b -> c : 2159175952016
+# c -> None
